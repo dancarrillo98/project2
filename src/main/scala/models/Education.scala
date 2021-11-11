20ea9bfdf7b39d1f606197f130ed7b93413495e0
DoctorRidley <bckoshy97@gmail.com>
2021-11-10 22:20:24 -0800

    Added documentation comments, formatted file

--- src/main/scala/models/Education.scala
+++ src/main/scala/models/Education.scala
@@ -1,6 +1,13 @@
 package models
 
+/**
+ * Stores data on people with high school degrees for specified geographic location
+ * @param t: Percentage of people with at least a high school degree.
+ * @param m: Percentage of males with at least a high school degree.
+ * @param f: Percentage of females with at least a high school degree.
+ */
 class Education(t: Double, m: Double, f: Double) {
+
     private val hs_degree: Double = t
     private val hs_degree_male: Double = m
     private val hs_degree_female: Double = f
