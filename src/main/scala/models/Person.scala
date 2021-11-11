c8f612ae04bd537142e31f066b6b27e83dad985a
DoctorRidley <bckoshy97@gmail.com>
2021-11-10 20:54:31 -0800

    Created model classes to help organize data

---
+++ src/main/scala/models/Person.scala
@@ -0,0 +1,9 @@
+package models
+
+/** This class organizes the data this app works with. It stores
+ * all of the data from the CSV file and/or Spark into a class
+ */
+class Person {
+    // FIXME: may have too many tuples to organize into a single class
+
+}
