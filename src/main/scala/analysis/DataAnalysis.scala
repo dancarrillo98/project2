2a3b626e2d7d281b226c8ad8681f265df0e8c5ba
Charpal <charles.dang1994@gmail.com>
2021-11-13 23:36:55 -0800

    Test AWS functionality

--- src/main/scala/analysis/DataAnalysis.scala
+++ src/main/scala/analysis/DataAnalysis.scala
@@ -7,9 +7,13 @@ import org.apache.spark.sql.DataFrame
 
 object DataApp{
     def main(args: Array[String]): Unit = {
-        //TODO Convert from Hortonworks Sandbox functionality to AWS EMR functionality
+        //Replace the localPath variable with your preferred destination
+        //Hortonworks SandBox
         val localPath = "file:///home/maria_dev/project2data/"
-        val bucketPath = "s3://alchemy-project-2/data/real_estate_db.csv"
+        //Begench S3
+        //val localPath = "s3://alchemy-project-2/data/"
+        //My Test S3
+        //val localPath = "s3://project2datanalyzer/data/"
 
         val spark: SparkSession = SparkSession
             .builder()
