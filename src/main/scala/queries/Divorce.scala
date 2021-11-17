73787d37982716cf358cc963621e8ddde88391d0
DoctorRidley <bckoshy97@gmail.com>
2021-11-16 20:44:56 -0800

    Added Spark data, fixed issue with DataFrame filtering

--- src/main/scala/queries/Divorce.scala
+++ src/main/scala/queries/Divorce.scala
@@ -1,38 +1,97 @@
 package queries
 
-import clients.SparkClient
-import org.apache.spark.sql.DataFrame
+import org.apache.spark.SparkContext
+import org.apache.spark.sql.{SparkSession, DataFrame}
+import org.apache.spark.sql.functions.avg
 
 object Divorce {
 
+    val spark: SparkSession = SparkSession.builder()
+      .master("local[1]")
+      .appName("DataAnalyzer")
+      .getOrCreate()
+
+    val sc: SparkContext = spark.sparkContext
+    sc.setLogLevel("ERROR")
+    import spark.implicits._
+
+
     def main(args: Array[String]): Unit = {
 
-        SparkClient.Connect()
+        val local_path: String = "/home/hdoop/Projects/project2/"
 
-        Query_2()
+        val df: DataFrame = spark.read
+          .option("header", true)
+          .option("inferSchema", true)
+          .option("delimiter", ",")
+          .csv(local_path + "src/main/resources/real_estate_db.csv")
 
-    }
-
-    def Query_1(): Unit = {
-
-        // How does the ratio between male and female correlate to divorce rates?
-        
-        //val columns: Array[String] = Array("male_pop", "female_pop", "divorced")
-        val df: DataFrame = SparkClient.Data()
-          .select("male_pop", "female_pop", "divorced")
-
-        df.show()
+        Query_1(df)
 
     }
 
-    def Query_2(): Unit = {
-        // How does wealth correlate with divorce?
-        // FIXME: what do with wealth
-        // TODO: get divorce rates
-        
-        val df: DataFrame = SparkClient.Data()
-          .select("family_mean", "family_median", "divorced")
+    def Query_1(df: DataFrame): Unit = {
 
-        df.show()
+        // How does the ratio between male and
+        // female correlate to divorce rates?
+        
+        // Population is not NaN, only 0
+        // checking for male and female population
+        // as NaN omits all the items for some reason
+        val data: DataFrame = df
+          .filter(
+            df("divorced") !== "NaN"
+          )
+          .select("state_ab", "city", "zip_code", "male_pop", "female_pop", "divorced")
+
+
+        // FIXME: city names are likely not unique b/w states
+        val data_state: DataFrame = data
+          .groupBy("state_ab")
+          .agg(avg("male_pop"), avg("female_pop"), avg("divorced"))
+
+        val data_city: DataFrame = data
+          .groupBy("city")
+          .agg(avg("male_pop"), avg("female_pop"), avg("divorced"))
+
+        val data_place: DataFrame = data
+          .groupBy("zip_code")
+          .agg(avg("male_pop"), avg("female_pop"), avg("divorced"))
+
+        data_state.show()
+        data_city.show()
+        data_place.show()
+
+    }
+
+    def Query_2(df: DataFrame): Unit = {
+        // How does income correlate with divorce?
+        
+        // Checking for multiple conditions with
+        // && throws an error for some reason
+        val data: DataFrame = df
+          .filter(
+            "family_median  != 'NaN' " +
+            "divorced != 'NaN'"
+          )
+          .select("state_ab", "city", "place", "family_median", "divorced")
+
+        // FIXME: city names are likely not unique b/w states
+        val data_state: DataFrame = df
+          .groupBy("state_ab")
+          .agg(avg("family_median"), avg("divorced"))
+
+        val data_city: DataFrame = df
+          .groupBy("city")
+          .agg(avg("family_median"), avg("divorced"))
+
+        val data_place: DataFrame = df
+          .groupBy("zip_code")
+          .agg(avg("family_median"), avg("divorced"))
+
+
+        data_state.show()
+        data_city.show()
+        data_place.show()
     }
 }
